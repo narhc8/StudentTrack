eb4b868a321a1b262868559f3230f58aa3d54082
CJ Murugapandi <murugapandic@vcu.edu>
2020-04-25 16:34:02 -0400

    Set up basic login page
    Set up a basic login page, will need to work out kinks in angular routing so page loads specified content based on if user is logged in or not (can be achieved with browser storage cache)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',  component: LoginComponent, pathMatch: 'full'},
+  {path: 'login',  component: LoginComponent, pathMatch: 'full'},
+  {path: 'signup',  component: SignupComponent, pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
