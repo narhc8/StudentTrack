305f9c74668d357f11f6d2c1aad54ccb4a1cc0e7
CJ Murugapandi <murugapandic@vcu.edu>
2020-04-28 22:03:59 -0400

    final commit apr28
    So basically added env variable for the api url base, so if --prod flag is used, the base url will be that of the server, if --prod flag is not used then localhost will be the default api url base.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  baseURL = 'http://3.234.246.29:8000';
+  baseURL = environment.base_API_URL;
   constructor(private http: HttpClient) { }
 
   testNodeServer() {
