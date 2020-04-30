062820320f353b898cf1aedb4cd7789f38f5ef3d
CJ Romanelli <romanellicj@vcu.edu>
2020-04-30 15:50:38 -0400

    Added board list

--- src/app/boards/boards.component.ts
+++ src/app/boards/boards.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-boards',
   templateUrl: './boards.component.html',
-  styleUrls: ['./boards.component.scss']
+  styleUrls: ['./boards.component.scss'],
 })
 export class BoardsComponent implements OnInit {
+  boards = ['Board 1', 'Board 2'];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.populateBoards();
   }
 
+  ngOnInit(): void {}
+
+  viewBoard(board: string) {
+    alert('Opening board ' + board);
+  }
+
+  populateBoards(): void {}
 }
