ffbde83c8b49d16b56bb0d64de1efb796efa192f
CJ Murugapandi <murugapandic@vcu.edu>
2020-04-29 23:10:58 -0400

    signup api endpoint added
    Now users can signup with the form on the /home/signup page and the user will populate in the users table correctly, error handling to add.

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+
 
 @Component({
   selector: 'app-signup',
@@ -7,11 +10,37 @@ import { Location } from '@angular/common';
   styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
-  constructor(private location: Location) {}
+  signupForm: FormGroup;
+
+  constructor(private location: Location, private formBuilder: FormBuilder, private auth: AuthService) {
+    this.signupForm = this.formBuilder.group({
+      first_name: [
+        ''
+      ],
+      last_name: [
+        ''
+      ],
+      email: [
+        ''
+      ],
+      username: [
+        ''
+      ],
+      password: [
+        ''
+      ]
+    });
+  }
 
   ngOnInit() {}
 
-  signupClick() {}
+  signupClick() {
+    const signupData = this.signupForm.value;
+    this.auth.signup(signupData).subscribe((data) => {
+      console.log(data);
+    });
+    console.log(signupData);
+  }
 
   goToHomePage() {
     this.location.back();
